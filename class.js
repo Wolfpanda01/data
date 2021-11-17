ef01b35240dbc6ed79c67d67b6ca12e8e09bb808
Wolfpanda01 <90583951+Wolfpanda01@users.noreply.github.com>
2021-11-17 11:18:25 -0700

    Add files via upload

---
+++ class.js
@@ -0,0 +1,57 @@
+class Rocket{
+constructor(
+    fuelType,
+    weight,
+    power,
+
+){
+    this.fuelType = fuelType;
+    this.weight = weight;
+    this.power = power;
+}
+launch(){
+    alert("launch sequence started");
+    return true;
+}
+landing(){
+    alert("landing sequence started");
+    return true;
+}
+detachThuruster(thurusterID){
+alert("Thuruster" + thurusterID + "has been detached.");
+return true;
+}
+
+}
+class Mission{
+    constructor(
+        objective,
+        launchDateDay,
+        launchDateMonth,
+        launchDateyear,
+        spaceCraft,
+        numAstornauts
+    ){this.objective =objective;
+        this. launchDateDay= launchDateDay;
+        this.launchDateMonth = launchDateMonth;
+        this. launchDateyear = launchDateyear;
+        this. spaceCraft=spaceCraft;
+        this.numAstornauts = numAstornauts
+    }
+displayMissionparameter(){
+    alert("the spacecraft" + this.spacecraft + "will launch on" + this.lauchDataDay + "of" + this.launchDateMonth + " " + this.launchdataYear)
+};
+    
+launch(){
+    alert("launch sequence started");
+}
+}
+    function LaunchFall() {
+    console.log("launchFall() started");
+    fallLaunch.launch();
+    }
+    function displayFall(){
+console.log(LaunchFall)
+    }
+
+  
